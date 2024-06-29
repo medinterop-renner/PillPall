6e6555568610b9fdca2ffa5d229ca7a760317b39
Paul Novak <paul.novak@fh-joanneum.at>
2024-06-29 22:36:49 +0200

    E-Mail button and help

--- app/src/main/java/com/example/pillpal420/Fragment_nav4.java
+++ app/src/main/java/com/example/pillpal420/Fragment_nav4.java
@@ -1,5 +1,7 @@
 package com.example.pillpal420;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +9,28 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 //Hilfe Fragment
 public class Fragment_nav4 extends Fragment {
+
+    private static final String EMAIL_STRING = "support@pillpal.at";
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_help, container, false);
+        View helpView = inflater.inflate(R.layout.fragment_help, container, false);
+
+
+        Button eMailBtn = helpView.findViewById(R.id.eMailBtn);
+
+        eMailBtn.setOnClickListener(v -> openMail());
+
+        return helpView;
+    }
+
+    private void openMail(){
+        Intent mailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", EMAIL_STRING, null));
+        mailIntent.putExtra(Intent.EXTRA_SUBJECT, "Support Request");
+        startActivity(Intent.createChooser(mailIntent, "Anbieter wählen:"));
     }
 }
