b0536f38c63bc0d3bb78e268d25468d3705a6685
Paul Novak <paul.novak@fh-joanneum.at>
2024-06-29 23:26:52 +0200

    Fragment 5 added

---
+++ app/src/main/java/com/example/pillpal420/Fragment_nav5.java
@@ -0,0 +1,25 @@
+package com.example.pillpal420;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class Fragment_nav5 extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View f5View = inflater.inflate(R.layout.fragment_nav5, container, false);
+
+        Button botBtn = f5View.findViewById(R.id.f5Button);
+        TextView botTextView = f5View.findViewById(R.id.f5TextView);
+        EditText botEditText = f5View.findViewById(R.id.f5editText);
+        return f5View;
+    }
+
+}
