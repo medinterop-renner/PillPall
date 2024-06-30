4fe5311e36f2aaa53f17c29741207754b0f08f11
Consti <consti.renner@gmail.com>
2024-06-30 21:25:09 +0200

    docs

--- app/src/main/java/com/example/pillpal420/backend/CorePatientDataCallback.java
+++ app/src/main/java/com/example/pillpal420/backend/CorePatientDataCallback.java
@@ -3,11 +3,18 @@ package com.example.pillpal420.backend;
 import com.example.pillpal420.backend.roomDB.CorePatientProfil;
 
 /**
- *
+ * Callback interface für das handling von corepatientdata operations in roomdb.
+ * Implementations von diesem interface werden dazu verwendet um asychronous database operations zu handlen im bezug auf corepatientprofiles.
  */
 public interface CorePatientDataCallback {
-
+    /**
+     * wird aufgerufen wenn corepatientprofile data erfolgreich aus der DB retrieved wird.
+     * @param patient loaded corepatientprofil
+     */
     void onPatientDataLoaded(CorePatientProfil patient);
 
+    /**
+     * wird aufgerufen wenn corepatientprofil data nicht in room db available ist.
+     */
     void onDataNotAvailable();
 }
